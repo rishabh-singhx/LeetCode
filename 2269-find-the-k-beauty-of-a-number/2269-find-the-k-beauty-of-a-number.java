5d0c92c76a75c8bf0c9f6e7fb5e5f5a7b61eacfd
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2022-11-26 08:38:55 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (87.35%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -1,20 +1,10 @@
 class Solution {
     public int divisorSubstrings(int num, int k) {
         String str=Integer.toString(num);
-        String arr[]=str.split("");
         int c=0;
-        String s="";
-        for(int i=0;i<k;i++)
+        for(int i=0;i<=str.length()-k;i++)
         {
-            s=s+arr[i];
-        }
-        int n=Integer.parseInt(s);
-        if(n!=0&&num%n==0)
-            c++;
-        for(int i=k;i<arr.length;i++)
-        {
-            s=s.substring(1)+arr[i];
-            n=Integer.parseInt(s);
+            int n=Integer.parseInt(str.substring(i,k+i));
             if(n!=0&&num%n==0)
                 c++;
         }
