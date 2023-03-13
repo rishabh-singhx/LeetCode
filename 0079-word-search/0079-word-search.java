d1bc7b1483e1f11aefb247af94464f510d36bc4d
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2023-03-13 14:10:16 +0530

    Time: 283 ms (23.33%), Space: 42.9 MB (8.40%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,49 +1,43 @@
 class Solution {
-    public boolean exist(char[][] board, String word) 
-    {
-        for (int i = 0; i < board.length; i++) 
-		{
-			for (int j = 0; j < board[0].length; j++)
-			{
-				if(board[i][j]==word.charAt(0))
-				{
-					boolean ans=Word_Search(board,i,j,word,0);
-					if(ans)
-					{
-						return ans;
-					}
-				}
-			}
-		}
-		return false;  
+    public boolean exist(char[][] board, String word) {
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                if(board[i][j]==word.charAt(0))
+                {
+                    boolean ans=search(board,i,j,word,0);
+                    
+                    if(ans==true)
+                        return ans;
+                }
+            }
+        }
+        return false;
     }
     
-    public static boolean Word_Search(char[][] maze, int cr, int cc, String word, int idx) 
-	{
-		if(idx==word.length())
-		{
-			return true;
-		}
-		
-		if(cr<0 || cc<0 || cr>=maze.length || cc>=maze[0].length || maze[cr][cc]!=word.charAt(idx))
-		{
-			return false;
-		}
-		
-		int []r= {-1,1,0,0};
-		int []c= {0,0,-1,1};
-		maze[cr][cc]='*';
-		boolean ans=false;
-		
-		for (int i = 0; i < c.length; i++) 
-		{
-			ans = Word_Search(maze, cr+r[i], cc+c[i], word, idx+1);
-			{
-				if(ans)
-					return ans;
-			}
-		}
-		maze[cr][cc]=word.charAt(idx);
-		return false;
-	}
+    public static boolean search(char[][] board,int i,int j,String word,int idx)
+    {
+        if(idx==word.length())
+        {
+            return true;
+        }
+        if(i<0 || j<0||i>=board.length || j>=board[0].length || board[i][j]!=word.charAt(idx))
+        {
+            return false;
+        }
+        int r[]={0,-1,0,1};
+        int c[]={-1,0,1,0};
+        board[i][j]='*';
+        boolean ans=false;
+        for(int k=0;k<r.length;k++)
+        {
+            ans=search(board,i+r[k],j+c[k],word,idx+1);
+            if(ans==true)
+                return ans;
+            
+        }
+        board[i][j]=word.charAt(idx);
+        return false;
+    }
 }
