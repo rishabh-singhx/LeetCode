57e9574d0326ed15410ba78aaff3a856463b1520
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-07-09 22:13:52 +0530

    Time: 5 ms (56.5%), Space: 43.2 MB (76.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
-            return false;
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char c : s.toCharArray()){
-            if(map.containsKey(c)){
-                map.put(c,map.get(c)+1);
-            }
-            else{
-                map.put(c,1);
-            }
+        // if(s.length()!=t.length())
+        //     return false;
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // for(char c : s.toCharArray()){
+        //     if(map.containsKey(c)){
+        //         map.put(c,map.get(c)+1);
+        //     }
+        //     else{
+        //         map.put(c,1);
+        //     }
+        // }
+        // for (char c : t.toCharArray()) {
+        //     if (!map.containsKey(c)) 
+        //         return false;
+        //     map.put(c, map.get(c) - 1);
+        //     if (map.get(c) < 0) 
+        //         return false;
+        // }
+        // return true;
+
+
+
+        if (s.length() != t.length()) return false;
+        int[] count = new int[26]; // Fixed size for a-z
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++; // increment for s
+            count[t.charAt(i) - 'a']--; // decrement for t
         }
-        for (char c : t.toCharArray()) {
-            if (!map.containsKey(c)) 
-                return false;
-            map.put(c, map.get(c) - 1);
-            if (map.get(c) < 0) 
-                return false;
+        for (int c : count) {
+            if (c != 0) return false; // imbalance found
         }
         return true;
     }
