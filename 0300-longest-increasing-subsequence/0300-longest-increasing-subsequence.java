a146db2165308ac51cafb72a0518aebb8072155b
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-07-21 09:38:42 +0530

    Time: 3 ms (95.85%), Space: 44.9 MB (26.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,36 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return LIS(nums);
+        return lis(nums);
     }
-    public static int LIS(int[] arr) {
-		int dp[] = new int[arr.length];
-		Arrays.fill(dp, 1);
-		for (int i = 1; i < dp.length; i++) {
-			for (int j = i-1; j >= 0; j--) {
-				if(arr[j]<arr[i]) {
-					int a = dp[j] + 1;
-					dp[i] = Math.max(dp[i], a);
-				}
+    private static int lis(int[] arr) {
+		int[] dp = new int[arr.length];
+		dp[0] = arr[0];
+ 		int len=1;
+ 		for (int i = 1; i < dp.length; i++) {
+			if(dp[len-1]<arr[i]) {
+				dp[len]=arr[i];
+				len++;
+			}
+			else {
+				int idx= BinarySearch(dp,0,len-1,arr[i]);
+				dp[idx] = arr[i];
 			}
 		}
-		int max=dp[0];
-		for (int i = 1; i < dp.length; i++) {
-			if(max<dp[i]) {
-				max=dp[i];
+		return len;
+	}
+
+	private static int BinarySearch(int[] dp, int si, int ei, int item) {
+		int idx = 0;
+		while(si<=ei) {
+			int mid = (si+ei)/2;
+			if(dp[mid]>=item) {
+				idx = mid;
+				ei = mid-1;
+			}
+			else {
+				si = mid+1;
 			}
 		}
-		return max;
+		return idx;
 	}
 }
