a2569a5a3826d23d694b683502c7f7260c50efe8
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-07-25 14:09:22 +0530

    Time: 24 ms (14.56%), Space: 51 MB (75.62%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,59 +1,29 @@
 class Solution {
-
     public int numDistinct(String s, String t) {
-        int[][] memo = new int[s.length()][t.length()];
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < t.length(); j++) {
-                memo[i][j] = -1;
+        int[][] dp = new int[s.length() + 1][t.length()+1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
             }
-        }
-        return countWays(s, t, 0, 0, memo);
+        }	
+        return DistinctSubsequences(s, t, 0, 0,dp);
     }
-
-    private int countWays(String s, String t, int i, int j, int[][] memo) {
-        // Base Case 1: t is fully matched
-        if (j == t.length()) return 1;
-
-        // Base Case 2: s is exhausted before t
-        if (i == s.length()) return 0;
-
-        // Memoized result
-        if (memo[i][j] != -1) return memo[i][j];
-
-        int ans = 0;
-
-        if (s.charAt(i) == t.charAt(j)) {
-            // Match case: use it or skip it
-            ans = countWays(s, t, i + 1, j + 1, memo) + countWays(s, t, i + 1, j, memo);
-        } else {
-            // No match: skip current char in s
-            ans = countWays(s, t, i + 1, j, memo);
+    public static int DistinctSubsequences(String s, String t, int i, int j,int[][] dp) {
+		if (t.length() == j) {
+			return 1;
+		}
+		if (i == s.length()) {
+			return 0;
+		}
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-
-        memo[i][j] = ans;
-        return ans;
-    }
-
-
-    //Bottom Up Approach
-    // public int numDistinct(String s, String t) {
-    //     int m = s.length();
-    //     int n = t.length();
-        
-    //     int[][] dp = new int[m + 1][n + 1];
-        
-    //     // Base case: empty t
-    //     for (int i = 0; i <= m; i++) dp[i][0] = 1;
-        
-    //     for (int i = 1; i <= m; i++) {
-    //         for (int j = 1; j <= n; j++) {
-    //             if (s.charAt(i - 1) == t.charAt(j - 1)) {
-    //                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-    //             } else {
-    //                 dp[i][j] = dp[i - 1][j];
-    //             }
-    //         }
-    //     }
-    //     return dp[m][n];
-    // }
+		int inc = 0;
+		int exc = 0;
+		if (s.charAt(i) == t.charAt(j)) {
+			inc = DistinctSubsequences(s, t, i + 1, j + 1,dp);
+		}
+		exc = DistinctSubsequences(s, t, i + 1, j,dp);
+		return dp[i][j] =  inc + exc;
+	}
 }
