0fc36e5b16a6f37f6d0fdff0bf4f178c4c22bc6b
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-07-29 20:09:00 +0530

    Time: 39 ms (32.89%), Space: 55.8 MB (69.76%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,52 @@
+public class Solution {
+    public int[] sortArray(int[] nums) {
+        heapSort(nums);
+        return nums;
+    }
+
+    private void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // One by one extract elements from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            swap(arr, 0, i);
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    private void heapify(int[] arr, int n, int i) {
+        int largest = i;       // Initialize largest as root
+        int left = 2 * i + 1;  // left = 2*i + 1
+        int right = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        // If right child is larger than largest so far
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        // If largest is not root
+        if (largest != i) {
+            swap(arr, i, largest);
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
