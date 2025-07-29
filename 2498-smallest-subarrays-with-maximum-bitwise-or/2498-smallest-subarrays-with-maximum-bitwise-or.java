272b0386bc25452ade1a4099b6ec1598275a9477
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-07-29 22:57:34 +0530

    Time: 25 ms (85.13%), Space: 61.8 MB (32.43%) - LeetHub

--- 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.java
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[] smallestSubarrays(int[] nums) {
         int n = nums.length;
-        int[] res = new int[n];
-        int[] lastSeen = new int[32]; // For each bit, track latest index where it's set
-
-        // Traverse from right to left
+        int[] result = new int[n];
+        int[] setBitIndex = new int[32]; // For each bit, track latest index where it's set
+        Arrays.fill(setBitIndex,-1);
         for (int i = n - 1; i >= 0; i--) {
-            // Update bit positions
-            for (int bit = 0; bit < 32; bit++) {
-                if ((nums[i] & (1 << bit)) != 0) {
-                    lastSeen[bit] = i;
+            //nums[i] = binary rep
+            int endIndex = i;
+            for (int j = 0; j < 32; j++) {
+                if ((nums[i] & (1 << j)) == 0) { //if jth bit is not set
+                    if (setBitIndex[j] != -1) {
+                        endIndex = Math.max(endIndex, setBitIndex[j]);
+                    }
+                } else {
+                    setBitIndex[j] = i;
                 }
             }
-
-            // Find furthest bit index we need to cover from i
-            int maxReach = i;
-            for (int bit = 0; bit < 32; bit++) {
-                maxReach = Math.max(maxReach, lastSeen[bit]);
-            }
-
-            res[i] = maxReach - i + 1;
+            result[i] = endIndex - i + 1;
         }
-
-        return res;
+        return result;
     }
 }
