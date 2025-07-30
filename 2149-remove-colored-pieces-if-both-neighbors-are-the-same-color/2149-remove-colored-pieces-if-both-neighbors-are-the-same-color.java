e0c48a2e445e66e2ab77ed461751e9f7ef229f77
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-07-30 16:13:07 +0530

    Time: 16 ms (51.99%), Space: 45.2 MB (69.23%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,39 +1,19 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
         
-        int c=0;
-        int movesA=0;
-        int movesB=0;
-        for(char ch:colors.toCharArray()){
-            if(ch=='A'){
-                c++;
-            }
-            else{
-                movesA += c>=3 ? c-2 : 0;
-                c=0;
-            }
-        }
-        if(c!=0){
-            movesA += c>=3 ? c-2 : 0;
-        }
-        c=0;
-        for(char ch:colors.toCharArray()){
-            if(ch=='B'){
-                c++;
-            }
-            else{
-                movesB += c>=3 ? c-2 : 0;
-                c=0;
+        int aMoves = 0, bMoves = 0;
+        int n = colors.length();
+        for (int i = 1; i < n - 1; i++) {
+            if (colors.charAt(i - 1) == colors.charAt(i) &&
+                colors.charAt(i + 1) == colors.charAt(i)) {
+                
+                if (colors.charAt(i) == 'A') {
+                    aMoves++;
+                } else {
+                    bMoves++;
+                }
             }
         }
-        if(c!=0){
-            movesB += c>=3 ? c-2 : 0;
-        }
-        // System.out.println(movesA+" "+movesB);
-        if(movesA==0)
-            return false;
-        if(movesA<=movesB)
-            return false;
-        return true;
+        return aMoves > bMoves;
     }
 }
