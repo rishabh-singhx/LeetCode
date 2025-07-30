f16215c01fab5028abd3fc3fe1142b245e2e4395
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-07-30 09:58:51 +0530

    Time: 17 ms (6.41%), Space: 43.4 MB (23.02%) - LeetHub

---
+++ 1933-number-of-different-integers-in-a-string/1933-number-of-different-integers-in-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numDifferentIntegers(String word) {
+        word = word.replaceAll("[a-z]", " ");
+        String[] parts = word.trim().split("\\s+");
+        Set<String> uniqueNums = new HashSet<>();
+        
+        for (String part : parts) {
+            System.out.println("*"+part+"*");
+            if (!part.isEmpty()) {
+                // Remove leading zeros
+                String num = part.replaceFirst("^0+", "");
+                if (num.isEmpty()) num = "0";  // Case for "000" etc.
+                uniqueNums.add(num);
+            }
+        }
+        return uniqueNums.size();
+    }
+}
