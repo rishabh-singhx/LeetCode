da3dd68e01be1d4bd3c10de43953bdd203a92c2e
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-08-01 10:12:38 +0530

    Time: 1 ms (82.66%), Space: 41.9 MB (79.28%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        int n=numRows;
-        List<List<Integer>> x = new ArrayList<List<Integer>>();
-		int row=0;
-		int star=1;
-		while(row<n)
-		{
-			int i=0;
-			int val=1;
-            List<Integer> rowss = new ArrayList();
-			while(i<star) 
-			{
-				rowss.add(val);
-				val=(row-i)*val/(i+1);
-				i++;
-			}
-			star++;
-			x.add(rowss);
-            row++;
-		}
-        return x;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        ans.add(first);
+        if(numRows==1)
+            return ans;
+        List<Integer> second = new ArrayList<>();
+        second.add(1);
+        second.add(1);
+        ans.add(second);
+        if(numRows==2)
+            return ans;
+        for(int i=2;i<numRows;i++){
+            List<Integer> x = new ArrayList<>();
+            x.add(1);
+            for(int j=0;j<ans.get(i-1).size()-1;j++){
+                x.add(ans.get(i-1).get(j)+ans.get(i-1).get(j+1));
+            }
+            x.add(1);
+            ans.add(x);
+        }
+        return ans;
     }
 }
