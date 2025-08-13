a060583b4c1aa8da3a34970eeca9eaa88c56bdf8
Rishabh Singh <101354419+rishabh-singhx@users.noreply.github.com>
2025-08-14 01:28:15 +0530

    Time: 7 ms (100%), Space: 44.5 MB (18.42%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,14 +1,18 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        long temp = n;
-        int i=0;
-        while(true){
-            if(temp == (long)Math.pow(3,i))
-                return true;
-            else if(temp < (long)Math.pow(3,i))
-                break;
-            i++;
-        }
-        return false;   
+        // long temp = n;
+        // int i=0;
+        // while(true){
+        //     if(temp == (long)Math.pow(3,i))
+        //         return true;
+        //     else if(temp < (long)Math.pow(3,i))
+        //         break;
+        //     i++;
+        // }
+        // return false; 
+
+
+        // Largest power of three in 32-bit int is 3^19 = 1162261467
+        return n > 0 && 1162261467 % n == 0;  
     }
 }
